b55f9741fe5d48a75f4d774c34ba5513cbae3a50
ImSota <sotahoriguchi1229@gmail.com>
2026-02-21 13:49:28 +0900

    Initial commit of ZaruBallv3 docs

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import Icons from "unplugin-icons/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://ImSota.github.io",
+  base: "/ZaruBallv3-docs",
+  integrations: [
+    starlight({
+      title: "ZaruBallv3",
+      customCss: ["./src/styles/custom.css"],
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/ImSota/" },
+        { icon: "x.com", label: "X", href: "https://x.com/sh80645614" },
+      ],
+      sidebar: [
+        {
+          label: "ビルドガイド",
+          autogenerate: { directory: "build-guide" },
+        },
+        {
+          label: "ファームウェア",
+          autogenerate: { directory: "firmware" },
+        },
+      ],
+    }),
+  ],
+  vite: {
+    plugins: [Icons({ compiler: "astro" })],
+  },
+});
