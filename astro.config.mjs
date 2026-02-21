649c725c5167c18afe3eb9a045d3983663fe3066
ImSota <sotahoriguchi1229@gmail.com>
2026-02-21 14:42:35 +0900

    Fix routing 404: Add trailingSlash: always and fix absolute link to include base ref

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import Icons from "unplugin-icons/vite";
 export default defineConfig({
   site: "https://ImSota.github.io",
   base: "/ZaruBallv3-docs",
+  trailingSlash: "always",
   integrations: [
     starlight({
       title: "ZaruBallv3",
